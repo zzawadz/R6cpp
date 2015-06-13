9a2488159e57383d483a74e41cb367c1180fadd5
Zygmunt Zawadzki <zawadzkizygmunt@gmail.com>
2015-06-13 10:55:04 +0200

    Some refactoring

--- R/createFiles.R
+++ R/createFiles.R
@@ -1,9 +1,9 @@
-rp_create_cpp = function(classes, path, out = "src")
+rp_create_files = function(classes, path, out = "src")
 {
-  lapply(classes, rp_create_cpp_export_file, path = path, out = out)
+  lapply(classes, rp_create_files_for_class, path = path, out = out)
 }
 
-rp_create_cpp_export_file = function(class, path, out = "src")
+rp_create_files_for_class = function(class, path, out = "src")
 {
   public = rp_extract_public(class)
   nameClass = rp_get_class_name(class)
@@ -14,16 +14,27 @@ rp_create_cpp_export_file = function(class, path, out = "src")
   paramsSlots       = NULL
   constructorsSlots = lapply(slotsList$constructors, rp_create_constructor_slot)
 
+  classList = list(nameClass = nameClass,
+       functionsSlots = functionsSlots,
+       paramsSlots = paramsSlots,
+       constructorsSlots = constructorsSlots)
 
-  constructorsCpp = lapply(constructorsSlots, rp_render_constructor_cpp) %>% paste(collapse = "")
-  functionsCpp = lapply(functionsSlots, rp_render_function_cpp) %>% paste(collapse = "")
+  # create cpp files
+  rp_create_cpp_export_file(classList, path, out)
+
+}
+
+rp_create_cpp_export_file = function(classList, path, out = "src")
+{
+  constructorsCpp = lapply(classList$constructorsSlots, rp_render_constructor_cpp) %>% paste(collapse = "")
+  functionsCpp = lapply(classList$functionsSlots, rp_render_function_cpp) %>% paste(collapse = "")
 
   header = stri_extract_last_regex(path, "\\w+\\.h")
   header = sprintf('#include "%s" \n', header)
 
   code = paste(header, constructorsCpp, functionsCpp)
 
-  out = file.path(out, paste0("R6cppExports",nameClass,".cpp"))
+  out = file.path(out, paste0("R6cppExports",classList$nameClass,".cpp"))
   cat(code, file = out)
 
 }
--- inst/other/simpleTests.R
+++ inst/other/simpleTests.R
@@ -1,25 +1,9 @@
 library(R6cpp)
-path  = "inst/other/R6Test.h"
-
-
-classes = rp_parse_cpp(path)
-
-class = classes[[1]]
-
-
-public = rp_extract_public(class)
-nameClass = rp_get_class_name(class)
-
-slotsList = rp_split_to_slots(public, nameClass)
-
-functionsSlots    = lapply(slotsList$functions, rp_create_function_slot, className = nameClass)
-paramsSlots       = NULL
-constructorsSlots = lapply(slotsList$constructors, rp_create_constructor_slot)
-
-
 path  = "inst/include/R6cpp.h"
 
+
 classes = rp_parse_cpp(path)
-class = classes[[1]]
+
+rp_create_files(classes, path)
 
 
